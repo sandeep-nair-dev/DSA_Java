4b4a852de223f704e20e2c54032197e662413706
Sandeep Nair <82336209+Hunterguns@users.noreply.github.com>
2024-12-04 23:57:14 +0530

    sn: Floyds cycle detection algorithm

---
+++ src/main/java/com/sandeep/java_dsa/Algorithms/FloydsCycleDetection.java
@@ -0,0 +1,25 @@
+package com.sandeep.java_dsa.Algorithms;
+
+public class FloydsCycleDetection {
+    public static int floydCycleDetection(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        int slow2 = 0;
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,4,2,2};
+        int duplicate = floydCycleDetection(arr);
+        System.out.println(duplicate);
+    }
+}
