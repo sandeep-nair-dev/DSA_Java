02f9b776a2e44e7ea824e27ecc952dc38e20974e
Sandeep Nair <82336209+Hunterguns@users.noreply.github.com>
2024-12-31 00:15:41 +0530

    dsa problem commit

---
+++ src/main/java/com/sandeep/java_dsa/Striver_A2Z_DSA_Sheet/Step4_BinarySearch/Lec_1/P4_1_11_NoOfTimesSortedArrayHasBeenRotated.java
@@ -0,0 +1,51 @@
+package com.sandeep.java_dsa.Striver_A2Z_DSA_Sheet.Step4_BinarySearch.Lec_1;
+
+public class P4_1_11_NoOfTimesSortedArrayHasBeenRotated {
+    public static int findKRotation(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            //search space is already sorted
+            //then arr[low] will always be
+            //the minimum in that search space:
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+                break;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+
+                // Eliminate left half:
+                low = mid + 1;
+            } else { //if right part is sorted:
+
+                // keep the minimum:
+                if (arr[mid] < ans) {
+                    index = mid;
+                    ans = arr[mid];
+                }
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int ans = findKRotation(arr);
+        System.out.println("The array is rotated " + ans + " times.");
+    }
+}
