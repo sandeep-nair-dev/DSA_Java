f4d487ee6e5cb06a6b777ff3b94ddb6df605367f
Sandeep Nair <82336209+Hunterguns@users.noreply.github.com>
2025-01-05 22:56:13 +0530

    dsa problem solution commit

--- src/main/java/com/sandeep/java_dsa/Striver_A2Z_DSA_Sheet/Step4_BinarySearch/Lec_1/P4_1_2_ImplementLowerBound.java
+++ src/main/java/com/sandeep/java_dsa/Striver_A2Z_DSA_Sheet/Step4_BinarySearch/Lec_1/P4_1_2_ImplementLowerBound.java
@@ -1,16 +1,16 @@
 package com.sandeep.java_dsa.Striver_A2Z_DSA_Sheet.Step4_BinarySearch.Lec_1;
 
 public class P4_1_2_ImplementLowerBound {
-    public static int lowerBound(int []arr, int n, int x) {
-        int lo=0, hi=n-1;
-        int ans=n;
-        while(lo<=hi){
-            int mid = (lo+hi)/2;
-            if(arr[mid]>=x){
-                ans=mid;
-                hi=mid-1;
-            }else{
-                lo=mid+1;
+    public static int lowerBound(int[] arr, int n, int x) {
+        int lo = 0, hi = n - 1;
+        int ans = n;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
             }
         }
         return ans;
--- src/main/java/com/sandeep/java_dsa/Striver_A2Z_DSA_Sheet/Step4_BinarySearch/Lec_1/P4_1_3ImplementUpperBound.java
+++ src/main/java/com/sandeep/java_dsa/Striver_A2Z_DSA_Sheet/Step4_BinarySearch/Lec_1/P4_1_3ImplementUpperBound.java
@@ -9,14 +9,14 @@ public class P4_1_3ImplementUpperBound {
     }
 
     private static int upperBound(int[] arr, int x, int n) {
-        int lo=0, hi=n-1, ans=-1;
-        while(lo<=hi){
-            int mid= (lo+hi)/2;
-            if(arr[mid]>x){
-                ans=mid;
-                hi=mid-1;
-            }else{
-                lo=mid+1;
+        int lo = 0, hi = n - 1, ans = -1;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (arr[mid] > x) {
+                ans = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
             }
         }
         return ans;
