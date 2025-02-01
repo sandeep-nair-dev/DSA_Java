5d3ccbcc11b52d9c0d284e06674cb6386d02b202
Sandeep Nair <82336209+Hunterguns@users.noreply.github.com>
2025-02-02 01:39:00 +0530

    problem solution commit: majority element

---
+++ src/main/java/com/sandeep/java_dsa/Striver_A2Z_DSA_Sheet/Step3_Arrays/Lec_2/P_3_2_3_MajoritiyElement.java
@@ -0,0 +1,21 @@
+package com.sandeep.java_dsa.Striver_A2Z_DSA_Sheet.Step3_Arrays.Lec_2;
+
+public class P_3_2_3_MajoritiyElement {
+    public int majorityElement(int[] nums) {
+        int freq = 1;
+        int curr = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == curr) {
+                freq++;
+            } else {
+                freq--;
+            }
+
+            if (freq == 0) {
+                curr = nums[i];
+                freq = 1;
+            }
+        }
+        return curr;
+    }
+}
